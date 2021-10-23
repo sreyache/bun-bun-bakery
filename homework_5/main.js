5b209d15b2b959c58545910ef84118ba84ede03a
sreyache <91205378+sreyache@users.noreply.github.com>
2021-10-23 17:37:51 -0400

    mostly working cart panel

--- homework_5/main.js
+++ homework_5/main.js
@@ -11,13 +11,25 @@ function glazeSelection() {
 function changeQuantityState() {
 }
 
-function move_navigation( $navigation, $MQ) {
-    if ( $(window).width() >= $MQ ) {
-       $navigation.detach();
-       $navigation.appendTo('header');
-    } else {
-       $navigation.detach();
-       $navigation.insertAfter('header');
-    }
- }
+// NEW CART TRIGGER OPTIONS 
 
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "500px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+// add item to cart 
+function addProductToCart() {
+    //open cart panel
+    openNav()
+
+    //add object to the cart panel
+
+    //update cart total=
+}
